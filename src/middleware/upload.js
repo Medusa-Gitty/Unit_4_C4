e1e411c6c92bcdc94b6cfc528540cdd2f66c4ba5
Sayak Mohanta <sayakmohanta1998@gmail.com>
2021-12-13 14:43:02 +0530

    update

---
+++ src/middleware/upload.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../uploads"));
+  },
+  filename: function (req, file, cb) {
+    const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniquePrefix + "-" + file.fieldname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+if()
+};
---
+++ src/models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    profile_url: [{ type: String, required: true }],
+    roles: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = userSchema;
