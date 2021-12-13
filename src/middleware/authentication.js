4a4edb26f853dfe03f5d485868965e7513f1bb22
Sayak Mohanta <sayakmohanta1998@gmail.com>
2021-12-13 16:40:16 +0530

    authtication

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -12,3 +12,29 @@ const verifyToken = (token) => {
     });
   });
 };
+
+module.exports = async (req, res, next) => {
+  //if we receive the token
+  const bearerToken = req?.headers?.authorization;
+  //if not or token is not bearer
+  if (!bearerToken || !bearerToken.startsWith("Bearer ")) {
+    return res
+      .status(403)
+      .json({ status: "failed", message: "Please provide a valid token" });
+  }
+  //try to get user frm token
+  const token = bearerToken.split(` `)[1];
+  let user;
+  try {
+    user = await verifyToken(token);
+  } catch (err) {
+    return res.status(400).json({ message: "Please provide a valid token" });
+  }
+  //If no user found then throw err
+  if (!user) {
+    return res.status(404).json({ message: "Please provide a valid token" });
+  }
+  //else req user
+  req.user = user;
+  return next();
+};
